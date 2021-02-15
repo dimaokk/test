69f1c6394010a52b454ed78683c385b9925519ca
wer <wer>
2021-02-15 21:47:39 +0700

    Initial Commit

---
+++ rew/rewApp.swift
@@ -0,0 +1,17 @@
+//
+//  rewApp.swift
+//  rew
+//
+//  Created by dima on 15.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct rewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
